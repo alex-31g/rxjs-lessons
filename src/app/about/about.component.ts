64441c658cf16b571ad325f8ae7ac1ae35e6a259
alex-31g <al.tsimbalyuk@gmail.com>
2021-01-30 06:49:18 +0200

    add

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -14,29 +14,5 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    const http$ = createHttpObservable('/api/courses');
-
-    // Создаем новый Observable на базе http$
-    const courses$ = http$
-      // До того как была выполнена подписка с помощью subscribe(), с потоком можно работать внутри rxjs-метода pipe()
-      // pipe() - позволяет чейнить несколько rxjs-операторов, чтобы создать новый Observable-поток
-      .pipe(
-        // map() - модифицирует значение из потока с помощью ф-ции-аргумента
-        map(res => res['payload'])
-      );
-
-    // Cоздание подписчика выполняется с помощью метода subscribe(), который принимает три метода-обработчика
-    courses$.subscribe(
-      // 1й метод - сработает, когда в Observable произойдет событие next()
-      courses => console.log(courses),
-
-      // 2й метод - сработает, когда в Observable произойдет событие-ошибка error()
-      // noop - rxjs оператор, который говорит, что в данной секции отсутствует операция
-      noop,
-
-      // 3й метод - сработает, когда в Observable произойдет событие complete()
-      () => console.log('completed')
-    )
-
   }
 }
