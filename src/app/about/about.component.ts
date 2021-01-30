21cbc996fa072d800f300228c5df63bd3f29163d
alex-31g <al.tsimbalyuk@gmail.com>
2021-01-30 07:16:53 +0200

    add

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { response } from 'express';
+import { concat, of } from 'rxjs';
 import { fromEvent, interval, noop, Observable, timer } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
@@ -14,5 +15,19 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
+    // of() - создает из аргументов Observable
+    const sourse1$ = of(10, 20, 30);
+    const sourse2$ = of(40, 50, 60);
+    const sourse3$ = of(70, 80, 90);
+
+    // Задача - объединить потоки sourse1$, sourse2$ и sourse3$
+    
+    // concat() - объединяет полученные Observable в один.
+    // Пока один из полученных Observable не завершится, concat не перейдет к следующему
+    const result$ = concat(sourse1$, sourse2$, sourse3$);
+
+    result$.subscribe(val => console.log(val)); // вывод в console - вариант 1
+    // result$.subscribe(console.log); // вывод в console - вариант 2
+    
   }
 }
