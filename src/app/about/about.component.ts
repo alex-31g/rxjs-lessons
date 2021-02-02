14a9dd29fb179b8be6f528fc3fdecbe7f6aeee9b
alex-31g <al.tsimbalyuk@gmail.com>
2021-02-03 01:59:36 +0200

    add

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -19,19 +19,7 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    // // of() - создает из аргументов Observable
-    // const sourse1$ = of(10, 20, 30);
-    // const sourse2$ = of(40, 50, 60);
-    // const sourse3$ = of(70, 80, 90);
 
-    // // Задача - объединить потоки sourse1$, sourse2$ и sourse3$
-    
-    // // concat() - объединяет полученные Observable в один.
-    // // Пока один из полученных Observable не завершится, concat не перейдет к следующему
-    // const result$ = concat(sourse1$, sourse2$, sourse3$);
-
-    // result$.subscribe(val => console.log(val)); // вывод в console - вариант 1
-    // // result$.subscribe(console.log); // вывод в console - вариант 2
    
 
 
