bc3397335ad88153a401d65f26b664211baaff4d
alex-31g <al.tsimbalyuk@gmail.com>
2021-02-10 04:01:38 +0200

    add

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, zip, from, ReplaySubject } from 'rxjs';
+import { concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, zip, from, ReplaySubject, BehaviorSubject } from 'rxjs';
 import { concatMap, delay, delayWhen, filter, map, take, tap, timeout } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 import { ajax } from 'rxjs/ajax';
@@ -13,45 +13,6 @@ export class AboutComponent implements OnInit {
   ngOnInit() {
 
 
-    // // Создание Subject - создать кастомный Observable
-    // const subject = new Subject();
 
-    // subject.next(10);
-    // subject.next(20);
-
-    // // Subject имеет те же методы, что и Observable - next, error, complete и другие.
-    // // Также Subject имеет pipe - поэтому его можно использовать с любыми rxjs-операторами
-    // // Subject должен быть приватным для той части приложения, которая эмитит данные.
-
-    // // Получить Observable из Subject
-    // // const series1$ = subject.asObservable();
-
-    // // series1$ эмитит данные из subject
-
-    // // После выполнения asObservable() - мы уже не можем использовать методы Subject'a - next, error, complete и другие,
-    // // мы можем только выполнить подписку на него
-    // // series1$.subscribe(console.log)
-
-
-    // subject.subscribe(console.log)
-
-
-    // subject.next(1);
-    // subject.next(2);
-    // subject.next(3);
-    // subject.complete();
-
-    
-
-    // // Обратите внимание, что еще один очень распространенный вариант использования наших просто субъектов - это многоадресная передача Multicasting.
-    // // В случае многоадресной рассылки мы хотим взять одно значение из одного наблюдаемого потока и повторно передать его в несколько отдельных выходных потоков.
-
-
-
-// ================================================
-
-
-// ================================================
-
-}
+  }
 }
